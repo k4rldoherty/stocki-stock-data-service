585eea89526251de69ec492410e9b378b79b87ba
k4rldoherty <karldoherty15@icloud.com>
2025-05-03 18:56:33 +0100

    removed https redirection while in dev

--- StockDataService/Program.cs
+++ StockDataService/Program.cs
@@ -9,17 +9,21 @@ builder.Services.AddMemoryCache();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.WebHost.UseUrls("http://0.0.0.0:8080");
 builder.Services.AddCors(opt =>
 {
-    opt.AddPolicy("OpenPolicy", policy =>
-    {
-        policy.AllowAnyHeader();
-        policy.AllowAnyMethod();
-        policy.AllowAnyOrigin(); // Change this when other services created. .WithOrigins("x", "y")
-    });
+    opt.AddPolicy(
+        "OpenPolicy",
+        policy =>
+        {
+            policy.AllowAnyHeader();
+            policy.AllowAnyMethod();
+            policy.AllowAnyOrigin(); // Change this when other services created. .WithOrigins("x", "y")
+        }
+    );
 });
 
 var app = builder.Build();
@@ -32,7 +36,7 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
 // app.UseAuthorization();
 
@@ -41,3 +45,4 @@ app.UseCors("OpenPolicy");
 app.MapControllers();
 
 app.Run();
+
